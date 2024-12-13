53eda33f49ddd8d1418e36e5a0797c5aab1061d0
Zerom Kim <zerom@robotis.com>
2024-12-13 18:33:43 +0900

    불필요 코드 제거

--- op3_gui_demo/src/main.cpp
+++ op3_gui_demo/src/main.cpp
@@ -40,8 +40,6 @@ int main(int argc, char **argv)
    **********************/
   QApplication app(argc, argv);
 
-  qRegisterMetaType<PreviewWalkingForm *>("PreviewWalkingForm");
-
   robotis_op::MainWindow w(argc, argv);
   w.show();
   app.connect(&app, SIGNAL(lastWindowClosed()), &app, SLOT(quit()));
